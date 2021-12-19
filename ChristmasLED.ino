34a0bf69a63c7427749e01bd7f7082528d4ff78b
jaycenornin <38961243+jaycenornin@users.noreply.github.com>
2021-12-19 14:23:00 -0700

    Tested and verified touch sensor control & interrupt (Fully functional code. Needs polish.)

--- ChristmasLED.ino
+++ ChristmasLED.ino
@@ -1,6 +1,29 @@
 /*
    Heyo!
    So this program uses an Arduino Xiao to light up an ARGB light string based on the WS2812b chip
+   Licensed by MIT license. I appreciate attribution if you use this code publicly. But it's not like I can stop you.
+
+   Explanation of function:
+    - This sketch works by first rate-limiting the loop() to run once every iLoopTime number of ms. 
+    - We set iFadeTime to the number of ms we want between each fade step in a "fade" effect.
+    - We calculate iFadeLoops to be the number of times loop() should run during iFadeTime.
+    - Each loop(), we check the current value of iLiteMode and run the corresponding ledModeX() function, then increment iTimer.
+    - Within each ledModeX() function is an if statement that mods iTimer over iFadeLoops so the subsequent commands only run every
+      iFadeLoops number of loop() cycles.
+      For example: 
+        - Set iLoopTime to 1ms
+        - Set iFadeTime to 100ms
+        - Set iFadeLoops to iFadeTime / iLoopTime
+        - Start iTimer at 0 and ++ every loop()
+        - loop() runs every 1ms
+        - ledModeX() runs every 1ms
+        - if (iTimer % iFadeLoops == 0) { runs every 100 loop()s which is every 100ms}
+        - If we set iLoopTime to 5ms and iFadeTime to 100ms, loop() runs every 5ms, ledModeX() runs every 5ms,
+        - and if(iTimer % iFadeLoops ==0) { runs every 20 loops()s which is every 100ms}        
+    - Each ledModeX() function calls a modeSetup() function and sets iFadeTime based on the best setting for that lite effect
+    - Each ledModeX() function has an explanation of how it works
+    - The iRed/iGreen/iBlue variables are set to bytes. This ensures they max at 255 and roll back over to 0. Useful for mapping
+      to a sin function and controlling LED brightness
    
    Important limitations:
     Current program uses ~14% of total available program space
@@ -13,10 +36,19 @@
     
     The light sensor works but is not calibrated.
 
-    Touch sensors are not tested (once upon a time "Prime" meant a 2 day celivery guarantee)
-      This also means the touchEvent() interrupt and function are untested
+    Touch sensors work, but you may want to customize the debounce timer.
+      Using interrupts gets you fast response when you tap, but apparently faulty edge detection and sensor bouncing create unexpected behavior.
+        Example: Even when using the FALLING interrupt, loop() halts on a press as though using CHANGE.
+        Example: Button presses may not debounce correctly and you'll run the ISR multiple times.
+      Barring unexpected performance problems, polling every loop may provide smoother operation, but you'll have to adjust loop and fade timers.
+
+    I'm not a fan of the use of delay() to handle effect timing. So far the only other option I've found is to use if + millis() + math to
+      run loop() at full speed, but there doesn't appear to be any way to get a resolution higher than 1ms so it's 6 vs a half dozen.
+      I could substitute micros() into that math for ~3μs resolution and I may explore that in the future. This would be valuable for replacing
+        interrupts with polling every loop() with low risk of missing an input event.
+        But it means a LOT of heavy-duty math to ensure the timing for lighting effects is accurate.
     
-    I use RGB instead of HSV cause after testing it was smoother. HSV had noticeable "stepping" and uggly shifting at the brightness I'm working at.
+    I use RGB instead of HSV cause after testing it was smoother. HSV had noticeable "stepping" and uggly shifting at the low brightness I'm working at.
 
     There's still some old code in here from various ideas I toyed with. This ain't clean by any stretech.
 
@@ -31,19 +63,22 @@
     mode 7 - Rainbow, 3 colors, 3 pixels, fading
     mode 8 - Blue/White, 2 colors, 3 pixels, chasing
     mode 9 - Blue/Purple, 2 colors, 3 pixels, chasing
-    mode 10 - Rotate through all modes every 10 seconds
+    mode 10 - Demo mode. Rotate through all modes every 10 seconds
 
    Future light modes to consider:
    x Warm yellow, candle flicker
    x Red/Green, moving (like fading, but with a 3rd intermediate color to give the illusion of linear motion)
    - Red/yellow/Green, fading, moving, fixed
    - Other holidays
+   - Fireflies
 
    To-Do
    x Additional light modes
    x Improved fading (longer pause between fade steps at min/max of each color, so they stay at red/green longer than the intermediate yellow)
-   - Second touch sensor for controlling color combinations and fade effects separately (mix and match!)
    x Change iLiteMax and adjust lighting modes to use iLiteMax/2 or /3 to keep the luminosity more even
+   - Long press on touch sensor for switching the unit full off or enabling demo mode iLiteMode(10)
+   - Second touch sensor for controlling color combinations and fade effects separately (mix and match!)
+   - Save the current lite mode to nonvolatile memory to restore prior settings on startup
    - Make chasing code more efficient - that's a LOT of function calls to make them work.
 */
 
@@ -51,30 +86,34 @@
 #include <FastLED.h>
 
 // For debugging
-// Comment out for standard operation
+// Set to NULL for normal operation
 // Set to 0 to turn off
 // Set to a non-zero positive integer to enable that light mode
 // Set to -1 to sequently test each LED at RGB(20)
-#define debugMode 10
+// Side note - NULL throws compiler warnings
+#define debugMode NULL
 
 // Constants
 #define cLedCount  16
-#define cLedControlPin 6
-#define cLiteSensorPin  7
+#define cLedControlPin 9
 #define cTouchSensorPin 8
-
-
+#define cLiteSensorPin  7
 
 // Configuration parameters - Constant
 #define cLightSwitch 50 // the "percent" value above which the device is off, and below which it is on
 #define iLiteMax 60 // Maximum RGB brightness per pixel, don't go over 100 per pixel for 16 pixel string
-#define iLoopTime 5 // ms delay between main loops
-#define iTotalLiteModes 7 // Number of LED lite modes
+#define iLoopTime 1 // ms delay between main loops, used with iFadeTime to ensure consistent timing of all light effects
+#define iTotalLiteModes 9 // Number of LED lite modes
+#define iDebounceTimer 2000 // number of ms for debouncing the touch input
 
 // Configuration parameters - Variable
-volatile short iLiteMode = 0; // LED mode (0=off), volatile for interrupts
+volatile short iLiteMode = 3; // LED mode (0=off), volatile for interrupts
 volatile boolean bModeSetup = 0; // Used to set up each lighting mode, reset by interrupts
-long iTimer = 0; // Timer used for tracking loops during certain fade cycles
+unsigned long int iReleaseTime = 0;
+//unsigned long int iPressTime = 0; // used for interrupts & button press timing
+//unsigned long int iLastPressTime = 0;
+unsigned long int iCurrentTime = 0;
+long iTimer = 0; // Timer used for counting the number of times loop() has run, used with iLoopTime and iFadeTime to regulate LED effects
 long iFadeTime = 100; // ms between fade cycles
 short iLightValue = 0; // value of ambient light read from sensor
 short iCycle = 0; // Repurposed for rotating between modes (ledMode10), 10 seconds
@@ -92,7 +131,7 @@ void setup() {
 
   // enable serial output when debugging
   // Disabled cause this started causing problems; unknown why but it causes the arduino to hang on start
-  if (debugMode > 0) {
+  if (debugMode) {
     Serial.begin(9600);
     /*while (!Serial) {
       ; // wait for Serial to be ready, probably the cause of the hang
@@ -106,7 +145,7 @@ void setup() {
   FastLED.addLeds<WS2812, cLedControlPin, GRB>(leds, cLedCount);
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 400);
   // initialize interrupts
-  attachInterrupt(digitalPinToInterrupt(cTouchSensorPin), touchEvent, FALLING); // react to an "onRelease" event from the touch sensor
+  attachInterrupt(digitalPinToInterrupt(cTouchSensorPin), touchReleaseEvent, FALLING); // react to an "onRelease" event from the touch sensor
 }
 
 void loop() {
@@ -143,6 +182,28 @@ void loop() {
     }
     }
   */
+
+  // trying to pull short press vs long press out of the interrupt
+  // gave up on this for now, slowly moving the code towards polling instead
+  // Keeping this old code cause it may be useful in polling short vs long as well
+  /*iCurrentTime = millis();
+  // Short Press
+  if (iPressTime - iCurrentTime < 2000 && digitalRead(cTouchSensorPin) == LOW)
+  {
+    iLiteMode++;
+    if (iLiteMode > iTotalLiteModes)
+    {
+      iLiteMode = 0;
+    }
+    bModeSetup = 0;
+    iLastPressTime = iPressTime;
+  }
+  else if (iPressTime - iCurrentTime > 2000 && iPressTime - iCurrentTime < 5000 && digitalRead(cTouchSensorPin) == HIGH)
+  {
+    iLiteMode = 10;
+    bModeSetup = 0;
+    iLastPressTime = iPressTime;
+  } */
 
   if (fLight < cLightSwitch) {
     statusOn();
@@ -188,9 +249,13 @@ void loop() {
     statusOff();
     ledMode0();
   }
+  // Increment iTimer
+  // This is used to count the number of times loop() has run
+  // Combined with iLoopTime and iFadeTime, this is used to regulate
+  // the speed of lighting effects.
   iTimer++;
-  //Serial.print("iTimer: " + iTimer);
-  delay(iLoopTime); // Main loop will run every iLoopTime
+  //Serial.println("iTimer: " + iTimer);
+  delay(iLoopTime); // Main loop will run every iLoopTime ms
 }
 
 /*********************
@@ -202,31 +267,42 @@ void loop() {
 // This is "supposed" to mimic an onReleased event
 // Each touch increments the LiteMode up to iTotalLiteModes modes, then reverts to 0
 // LiteMode 0 = Off
-void touchEvent() {
-  iLiteMode++;
-  if (iLiteMode > iTotalLiteModes) {
-    iLiteMode = 0;
+// Calling this on an interrupt works, but has undesirable side effects 
+// (appears to be improper edge detection in the arduino "FALLING" triger)
+// (calling noInterrupts() from within the ISR could be fun to try)
+void touchReleaseEvent() {
+  static unsigned long iLastPressTime = 0;
+  iReleaseTime = millis();
+  if (iReleaseTime - iLastPressTime > iDebounceTimer) {
+    iLiteMode++;
+    if (iLiteMode > iTotalLiteModes)
+    {
+      iLiteMode = 0;
+    }
+    bModeSetup = 0;
+    iLastPressTime = iReleaseTime;
   }
-  bModeSetup = 0;
 }
 
-// blinks the built-in LED to indicate the program is running
+
+// lights the built-in LED to indicate the program is running
 void statusOn() {
   //ledBlink(1500,250);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-// blinks the built-in LED to indicate the program is idle
+// lights the built-in LED to indicate the program is idle
 void statusOff() {
   //ledBlink(250,1500);
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 // blinks the built-in LED based on onDelay and offDelay
+// Note: XIAO ties LOW to LED-ON
 void ledBlink(short onDelay, short offDelay) {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(onDelay);
   digitalWrite(LED_BUILTIN, LOW);
+  delay(onDelay);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(offDelay);
 }
 
@@ -242,14 +318,14 @@ void ledTest() {
 
 // generic function to set up variables for each lighting mode
 void modeSetup(int redMax, int greenMax, int blueMax, int red, int green, int blue, int fadeTime) {
-  iRedMax = redMax;
+  iRedMax = redMax; // Maximum value for red LED
   iGreenMax = greenMax;
   iBlueMax = blueMax;
-  iRed = red;
+  iRed = red; // Current value for red LED
   iGreen = green;
   iBlue = blue;
-  iFadeTime = fadeTime;
-  iFadeLoops = iFadeTime / iLoopTime; // Do this each loop in
+  iFadeTime = fadeTime; // The fade time value appropriate for the calling light effect
+  iFadeLoops = iFadeTime / iLoopTime; // recalculate iFadeLoops with the updated iFadeTime
   bModeSetup = 1;
 }
 
@@ -275,7 +351,7 @@ void ledMode1() {
   if (bModeSetup == 0) {
     modeSetup(iLiteMax/2, iLiteMax/4, 0, 255, 127, 0, 1000); 
   }
-  if (iTimer % iFadeLoops == 0) { // No need to run this often since it's static
+  if (iTimer % iFadeLoops == 0) { // No need to run this often since it's a static effect
     byte iRTemp = map(iRed, 0, 255, 0, iRedMax);
     byte iGTemp = map(iGreen, 0, 255, 0, iGreenMax);
     byte iBTemp = map(iBlue, 0, 255, 0, iBlueMax);
@@ -594,3 +670,4 @@ void ledMode10() {
     bModeSetup = 0;
   }
 }
+
